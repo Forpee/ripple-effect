afbdca5c30c6da177fa8e57ebe7880f39b84aa1d
Forpee <khalilhassam3@gmail.com>
2022-04-08 15:26:43 +0200

    yes

--- src/script.js
+++ src/script.js
@@ -77,6 +77,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// renderer.setClearColor(0xff0000, 1)
 
 /**
  * Animate
