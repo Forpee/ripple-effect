435faf4e524488fb30af6497c5406959c6b4b301
Forpee <khalilhassam3@gmail.com>
2022-04-08 17:24:21 +0200

    update

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -2,9 +2,13 @@ uniform sampler2D uTexture;
 uniform sampler2D uDisplacement;
 varying vec2 vUv;
 
-
 void main()
 {
-    vec4 color = texture2D(uTexture, vUv);
-    gl_FragColor = color;
+    vec4 displacement=texture2D(uDisplacement,vUv);
+    float theta=displacement.r*2.*3.1415926535897932384626433832795;
+    vec2 dir = vec2(sin(theta),cos(theta));
+
+    vec2 uv = vUv + dir*displacement.r;
+    vec4 color=texture2D(uTexture,uv);
+    gl_FragColor=color;
 }
